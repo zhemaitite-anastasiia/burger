9e966d4b46dd0066017591afe9191b6947883863
AnastasiiaZhemaitite <a.zhemaitite@gmail.com>
2018-03-05 14:56:10 -0600

    Sql table created and burgers added

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Bleu Ribbon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Banzai Burger');
